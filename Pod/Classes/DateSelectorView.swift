fa13bdf8626a1cf9b6a5210971efab05221795b3
Erez Haim <erez@livemed.co>
2015-09-02 13:36:47 +0300

    setting frame on init

--- Pod/Classes/DateSelectorView.swift
+++ Pod/Classes/DateSelectorView.swift
@@ -213,7 +213,7 @@ public class DateSelectorView: UIView, UIScrollViewDelegate {
     
     func createDayView(date: NSDate) -> DayView{
         let dayView = NSBundle(forClass: DateSelectorView.self).loadNibNamed("DayView", owner: self, options: nil)[0] as! DayView
-        //dayView.frame = CGRectMake(0, 0, dayWidth, scrollView.frame.height)
+        dayView.frame = CGRectMake(0, 0, dayWidth, scrollView.frame.height)
         dayView.layer.borderColor = self.borderColor.CGColor
         dayView.layer.borderWidth = self.borderWidth
         dayView.selectedBackgroundColor = self.selectedDayBackgroundColor
